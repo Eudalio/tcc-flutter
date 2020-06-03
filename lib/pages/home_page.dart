bfd19a8ae9f2663db86ab39615fbd63e9ab25746
Eudalio Sousa <tonnydallios@gmail.com>
2020-06-02 21:22:57 -0300

    home screen completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,103 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(top: 20.0, right: 10.0),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: Image.asset(
+                          "assets/flutter-logo2.png",
+                          fit: BoxFit.cover,
+                          height: 140.0,
+                          width: 150.0,
+                        ),
+                      ),
+                      Text(
+                        "Flutter",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15.0),
+                      )
+                    ],
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, left: 10.0),
+                      child: Image.asset(
+                        "assets/react-native-logo.png",
+                        fit: BoxFit.cover,
+                        height: 148.0,
+                        width: 150.0,
+                      ),
+                    ),
+                    Text(
+                      "React Native",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15.0),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      height: 50.0,
+                      width: 270.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Consultar API - GitHub",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                        color: Colors.red,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      height: 50.0,
+                      width: 270.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Tirar uma foto",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                        color: Colors.red,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      height: 50.0,
+                      width: 270.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Localização",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                        color: Colors.red,
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
