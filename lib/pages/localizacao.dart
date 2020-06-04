47e7e029cfce25319ee7f134ec3fd00d44fe6166
Eudalio Sousa <tonnydallios@gmail.com>
2020-06-03 22:33:32 -0300

    Location screen completed

--- lib/pages/localizacao.dart
+++ lib/pages/localizacao.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Localizacao extends StatefulWidget {
   @override
@@ -6,14 +9,33 @@ class Localizacao extends StatefulWidget {
 }
 
 class _LocalizacaoState extends State<Localizacao> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-5.193380, -39.280718),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Localização"),
-        centerTitle: true,
-        backgroundColor: Color(0xFFF42B2B),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Localização"),
+          centerTitle: true,
+          backgroundColor: Color(0xFFF42B2B),
+        ),
+        body: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
   }
 }
